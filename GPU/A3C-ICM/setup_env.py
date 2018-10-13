832162d69dffd6cf0e229366f0349499db3f2b26
its_pores <42297782+Ameyapores@users.noreply.github.com>
2018-10-13 17:25:41 +0100

    Update setup_env.py

--- GPU/A3C-ICM/setup_env.py
+++ GPU/A3C-ICM/setup_env.py
@@ -1,13 +1,13 @@
 
 import gym
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv, wrap as nes_py_wrap
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 import gym_super_mario_bros
 import numpy as np
 
 def setup_env(env_id: str) -> gym.Env:
     env = gym_super_mario_bros.make(env_id)
-    env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+    env = BinarySpaceToDiscreteSpaceEnv(env, COMPLEX_MOVEMENT)
     #print(env.observation_space, env.action_space)
     env = nes_py_wrap(env, death_penalty= -1 ,agent_history_length = 1)
     #a= np.zeros(env.observation_space.shape)
